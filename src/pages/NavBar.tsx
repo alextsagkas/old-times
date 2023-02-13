40f4802a9406dd66555aceb9b5ce480ae9072bc8
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2023-02-14 00:05:54 +0200

    navbar animations

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 import PageComponent from "../helperComponents/PageComponent";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { NavBarProps, RefsItemInterface } from "../../types/types";
 
 const NavBar = ({ children, refs, scrollPosition }: NavBarProps) => {
   const [title, setTitle] = useState("Menu");
   const [isVisible, setIsVisible] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    if (window !== undefined) {
+      setWindowWidth(window.innerWidth);
+    }
+  }, []);
 
   const refShorTitle = (title: string): string => {
-    if (title === "Traditional Greek dishes" && window.innerWidth < 768) {
+    if (title === "Traditional Greek dishes" && windowWidth < 768) {
       return "Traditional";
-    } else if (title === "Ice cream and milkshake" && window.innerWidth < 768) {
+    } else if (title === "Ice cream and milkshake" && windowWidth < 768) {
       return "Ice cream";
     } else {
       return title;
@@ -23,12 +29,20 @@ const NavBar = ({ children, refs, scrollPosition }: NavBarProps) => {
     refs.map((ref: RefsItemInterface) => {
       if (
         ref.ref.current?.getBoundingClientRect() !== undefined &&
-        ref.ref.current?.getBoundingClientRect().top < 7
+        ref.ref.current?.getBoundingClientRect().top < 7 &&
+        !isVisible
       ) {
         setTitle(refShorTitle(ref.title));
       }
+
+      if (isVisible) {
+        setTitle("Menu");
+      }
     });
-  }, [scrollPosition]);
+  }, [scrollPosition, isVisible]);
+
+  const hamburgerLine =
+    "h-[3px] w-6 my-[2.5px] rounded-full bg-primary transition-all ease-in-out opacity-100 duration-[500ms]";
 
   return (
     <>
@@ -39,73 +53,87 @@ const NavBar = ({ children, refs, scrollPosition }: NavBarProps) => {
       >
         <PageComponent>
           <div className="grid grid-cols-3 items-center py-1">
-            <div className="text-normal justify-self-start font-vujahddayScript font-bold text-primary">
+            <nav className="text-normal justify-self-start font-vujahddayScript font-bold text-primary">
               {title}
-            </div>
-            <button
-              onClick={(event) => {
-                event.preventDefault();
-                if (refs[0] !== undefined) {
-                  refs[0].ref.current?.scrollIntoView({ behavior: "smooth" });
-                }
-              }}
-              className="justify-self-center"
-            >
-              <Image
-                src="/old_times_transparent.png"
-                alt="old-times-logo"
-                width={150}
-                height={150}
-                className="h-8 w-8"
-              />
-            </button>
-            {isVisible ? (
-              <XMarkIcon
+            </nav>
+            <nav className="h-8 justify-self-center">
+              <button
                 onClick={(event) => {
                   event.preventDefault();
                   setIsVisible(false);
+                  if (refs[0] !== undefined) {
+                    refs[0].ref.current?.scrollIntoView({ behavior: "smooth" });
+                  }
                 }}
-                className="h-7 w-7 justify-self-end text-primary"
-              />
-            ) : (
-              <Bars3Icon
+              >
+                <Image
+                  src="/old_times_transparent.png"
+                  alt="old-times-logo"
+                  width={150}
+                  height={150}
+                  className="my-0 h-8 w-8 py-0"
+                />
+              </button>
+            </nav>
+            <nav className="flex flex-row justify-end">
+              <button
+                className="flex cursor-pointer flex-col items-center justify-center outline-none"
                 onClick={(event) => {
                   event.preventDefault();
-                  setIsVisible(true);
+                  setIsVisible(!isVisible);
                 }}
-                className="h-7 w-7 justify-self-end text-primary"
-              />
-            )}
+              >
+                <div
+                  className={`${hamburgerLine} ${
+                    isVisible && "translate-y-[8px] rotate-45"
+                  }`}
+                />
+                <div
+                  className={`${hamburgerLine} ${
+                    isVisible && "origin-center scale-x-0 opacity-0"
+                  }`}
+                />
+                <div
+                  className={`${hamburgerLine} ${
+                    isVisible && "-translate-y-[8px] -rotate-45"
+                  }`}
+                />
+              </button>
+            </nav>
           </div>
         </PageComponent>
-        {isVisible && (
-          <div className="h-screen bg-primary">
-            <PageComponent>
-              <div className="text-normal grid h-fit grid-cols-2 justify-items-start gap-y-2 py-2 font-vujahddayScript">
-                {refs.map((ref) => {
-                  if (ref.title !== "Menu") {
-                    return (
-                      <div
-                        key={ref.title}
-                        className="w-full text-orange-100"
-                        onClick={(event) => {
-                          event.preventDefault();
-                          setIsVisible(false);
-                          ref.ref.current?.scrollIntoView({
-                            behavior: "smooth",
-                          });
-                        }}
-                      >
-                        {refShorTitle(ref.title)}
-                      </div>
-                    );
-                  }
-                })}
-              </div>
-            </PageComponent>
-          </div>
-        )}
       </div>
+      <section
+        className={`fixed top-10 h-full w-full origin-top bg-primary transition-all duration-[400ms] ease-out ${
+          isVisible ? "scale-y-100" : "scale-y-0"
+        }`}
+      >
+        <PageComponent>
+          <div className="text-normal grid h-fit grid-cols-2 justify-items-start gap-y-2 py-2 font-vujahddayScript">
+            {refs.map((ref) => {
+              if (ref.title !== "Menu") {
+                return (
+                  <nav
+                    key={ref.title}
+                    className={`w-full cursor-pointer text-orange-100 transition-all duration-100 ease-out ${
+                      isVisible ? "opacity-100" : "opacity-0"
+                    }`}
+                    onClick={(event) => {
+                      event.preventDefault();
+                      setIsVisible(false);
+                      ref.ref.current?.scrollIntoView({
+                        behavior: "smooth",
+                      });
+                    }}
+                  >
+                    {refShorTitle(ref.title)}
+                  </nav>
+                );
+              }
+            })}
+          </div>
+        </PageComponent>
+      </section>
       {children}
     </>
   );
