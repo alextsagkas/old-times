e6053fa9e68e8838c791bc6148967be6b4a1bd2d
Alexandros Tsagkaropoulos <alextsagkas2001@gmail.com>
2024-08-05 19:57:03 +0300

    Remove console logging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,10 +47,6 @@ const Home: NextPage<HomeProps> = () => {
     };
   }, [scrollPosition]);
 
-  categories.map((category, index) => {
-    console.log(index, category.category);
-  });
-
   return (
     <>
       <Head>
