b447dac137a9af47bd567c07fcb8bb97f8c7a67b
Alexandros Tsagkaropoulos <alextsagkas2001@gmail.com>
2024-08-05 22:09:54 +0300

    Three columns in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -105,7 +105,7 @@ const NavBar = ({ children, refs, scrollPosition }: NavBarProps) => {
         }`}
       >
         <PageComponent>
-          <div className="grid h-fit grid-cols-2 font-normal justify-items-start gap-y-5 py-4 font-serif text-xl">
+          <div className="grid h-fit grid-cols-3 font-normal justify-items-start gap-y-5 py-4 font-serif text-xl">
             {refs.map((ref) => {
               if (ref.title !== "Menu") {
                 return (
